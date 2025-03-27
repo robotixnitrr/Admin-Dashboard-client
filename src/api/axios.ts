4d1f682598875761ac86faa5a31a818e91931c21
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2025-03-27 14:42:35 +0530

    updates

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Replace with your server URL
+    baseURL: import.meta.env.API_BASE_URL || 'http://localhost:5000/api', // Replace with your server URL
     timeout: 10000, // Optional: Set a timeout for requests
     headers: {
         'Content-Type': 'application/json',
