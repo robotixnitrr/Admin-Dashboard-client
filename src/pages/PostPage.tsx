2572509af54005d697de3d8487d7361781face0a
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2025-03-27 14:31:15 +0530

    updates

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom'; // For navigation to individual blog posts
-import { Blog } from '../types/Blog.ts'; // Import the Blog interface
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom"; // For navigation to individual blog posts
+import { Blog } from "../types/Blog.ts"; // Import the Blog interface
 
-const API_BASE_URL = 'http://localhost:5000/api'; // Update base URL
-
+const API_BASE_URL = "http://localhost:5000/api"; // Update base URL
 
 function PostPage() {
   const [blogs, setBlogs] = useState<Blog[]>([]); // Use the Blog interface for state
@@ -15,7 +14,7 @@ function PostPage() {
         const response = await axios.get<Blog[]>(`${API_BASE_URL}/post`); // Specify the response type
         setBlogs(response.data);
       } catch (error) {
-        console.error('Error fetching blogs:', error);
+        console.error("Error fetching blogs:", error);
       }
     };
 
@@ -29,11 +28,21 @@ function PostPage() {
         {blogs.map((blog) => (
           <div className="col-md-4 mb-4" key={blog._id}>
             <div className="card">
-              <img src={blog.imageUrl} className="card-img-top" alt={blog.title} /> {/* Assuming each blog has an image */}
+              <img
+                src={blog.imageUrl}
+                className="card-img-top"
+                alt={blog.title}
+              />{" "}
+              {/* Assuming each blog has an image */}
               <div className="card-body">
                 <h5 className="card-title">{blog.title}</h5>
-                <p className="card-text">{blog.content.substring(0, 100)}...</p> {/* Display a snippet of the content */}
-                <Link to={`/blogs/${blog._id}`} className="btn btn-primary">Read More</Link>
+                <p className="card-text">
+                  {blog.content.substring(0, 100)}...
+                </p>{" "}
+                {/* Display a snippet of the content */}
+                <Link to={`/blogs/${blog._id}`} className="btn btn-primary">
+                  Read More
+                </Link>
               </div>
             </div>
           </div>
