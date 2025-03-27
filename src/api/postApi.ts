3e5cca17d966d86200fb1cc6994f1fad85f88555
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2025-03-27 14:46:03 +0530

    updates

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -1,8 +1,10 @@
 import { AxiosResponse } from "axios";
 import { Blog } from "../types/Blog";
 import api from "./axios";
-const API_BASE_URL = 'http://localhost:5000/api/post';
+const API_BASE_URL = `${import.meta.env.API_BASE_URL}/post`;
 
-export const createPost = (postData: Partial<Blog>): Promise<AxiosResponse<Blog>> => {
-    return api.post<Blog>(`${API_BASE_URL}`, postData);
+export const createPost = (
+  postData: Partial<Blog>
+): Promise<AxiosResponse<Blog>> => {
+  return api.post<Blog>(`${API_BASE_URL}`, postData);
 };
